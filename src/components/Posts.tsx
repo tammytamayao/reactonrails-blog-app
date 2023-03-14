34179f4cd0f4b79c7ff65c6701857ef8220f034e
Princess Tamayao <princess@bpoc.co.jp>
2023-03-14 10:31:03 +0800

    added like feature for posts

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,15 +1,35 @@
 import React, { useState, useEffect} from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {baseURL, client} from "../config/AxiosConfig";
 
 const Posts = () => {
 
+  const navigate = useNavigate();
   const [posts, setPosts] = useState<any[]>([]);
+  
+  const getLikeCount = async (post_id:any) => {
+    const response: any = await client.get(baseURL+`/posts/${post_id}/likers/show`);
+    if(response.status!==200){
+      alert('Like not counted. Try Again.')
+    }
+  }
+
+  const likePost = async (post_id:any) => {
+    const response: any = await client.post(baseURL+`/posts/${post_id}/likers/create`);
+    if(response.status===200) {
+      navigate("/posts")
+      getLikeCount(post_id);
+      alert('Post liked');
+      window.location.reload();
+    } else {
+      alert('Post not liked. Try Again.')
+    }
+  }
 
   const getAllPosts = async () => {
     const response: any = await client.get(baseURL+`/posts`);
     if(response.status===200) {
-      setPosts(response.data)
+      setPosts(response.data);
     }
   }
 
@@ -21,10 +41,13 @@ const Posts = () => {
   }, []);
 
   const allPosts = posts.map((post, index) => (
+    
     <div key={index}>
         <div>
-          <span>Title: {post.title}</span><br/>
-          <span><Link to={`/posts/${post.id}`}>View Post</Link></span><br/>
+          <span><b>Title: {post.title}</b></span><br/>
+          <span>Total Likes: { post.likes==null ? 0 : post.likes}</span><br/>
+          <button type="button" className="btn btn-danger" onClick={()=>likePost(post.id)}>Like</button> | 
+          <span><Link to={`/posts/${post.id}`}> View Post</Link></span><hr/>
           <p></p>
         </div>
     </div>
@@ -32,7 +55,7 @@ const Posts = () => {
 
   return (
     <>
-        <h1>ALL POSTS</h1>
+        <h1><b>ALL POSTS</b></h1>
         <Link to={`/posts/new`}>Add New Post</Link>
         <hr/>
         <div> {allPosts} </div>
